736b3a8133fc62f86ad34f55a2c92a8ac32e87bc
Victoria-Kravets <victoriakravets93@gmail.com>
2017-07-28 22:21:07 +0300

    - added opportunity to delete objects

--- WorkWithRealm/MyTableViewController.swift
+++ WorkWithRealm/MyTableViewController.swift
@@ -23,15 +23,15 @@ class MyTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let defaultPath = Realm.Configuration.defaultConfiguration.fileURL
-       // try FileManager.defaultManager.removeItemAtPath(defaultPath)
+        // try FileManager.defaultManager.removeItemAtPath(defaultPath)
         
         populateDefaultResipes()
-                
+        
         
     }
     
     func populateDefaultResipes() {
-     print(resipes.count)
+        print(resipes.count)
         if resipes.count == 0 { // if count equal 0, it means that cotegory doesn't have any record
             
             try! realm.write() { // adding records to database
@@ -48,24 +48,20 @@ class MyTableViewController: UITableViewController {
             resipes = realm.objects(Resipe.self) // request all creating categories
         }
     }
-
+    
     // MARK: - Table view data source
-
-    @IBAction func addNewResipe(_ sender: Any) {
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "goToNextVC")
-        self.navigationController?.present(vc!, animated: true, completion: nil)
-    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         print(resipes.count)
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return resipes.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as? ResipeTableViewCell {
@@ -83,50 +79,50 @@ class MyTableViewController: UITableViewController {
         selectedResipe = resipes[indexPath.row]
         return indexPath
     }
-
-    /*
-    // Override to support conditional editing of the table view.
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+        
         return true
     }
-    */
-
-    /*
+    
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            try! realm.write() {
+                realm.delete(self.resipes[indexPath.row])
+            }
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }// else if editingStyle == .insert {
+        //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        //        }
     }
-    */
-
+    
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
